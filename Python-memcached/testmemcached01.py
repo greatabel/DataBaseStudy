f35e2c67641d8ca23ef798ec54d42d8ba8cf450e
greatabel <greatabel1@126.com>
2015-06-17 10:06:24 +0800

    memcached

---
+++ Python-memcached/testmemcached01.py
@@ -0,0 +1,9 @@
+#http://stackoverflow.com/questions/28851037/pip-install-python3-memcached-fails
+#http://my.oschina.net/flynewton/blog/10660
+
+import memcache
+ 
+mc = memcache.Client(['127.0.0.1:12000'],debug=0)
+mc.set("foo","bar")
+value = mc.get("foo")
+print(value)
