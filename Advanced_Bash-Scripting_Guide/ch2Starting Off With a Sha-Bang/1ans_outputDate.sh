8610ad73964f88125e65ca987677ad725295459c
greatabel <greatabel1@126.com>
2015-07-28 00:34:46 +0800

    exercise 1

--- Advanced_Bash-Scripting_Guide/ch2Starting Off With a Sha-Bang/1ans_outputDate.sh
+++ Advanced_Bash-Scripting_Guide/ch2Starting Off With a Sha-Bang/1ans_outputDate.sh
@@ -1,1 +1,21 @@
+LOG_DIR="$Home/Downloads/AbelProject/Transact-SQL/Advanced_Bash-Scripting_Guide/ch2Starting Off With a Sha-Bang"
+mydate="$(date)"
+echo "1 $mydate"
 
+
+file=exists_and_writeable.txt
+
+if [ ! -e "$file" ] ; then
+    touch "$file"
+fi
+
+if [ ! -w "$file" ] ; then
+    echo "cannot write to $file"
+    exit 1
+fi
+
+
+echo $LOG_DIR
+cd $LOG_DIR
+echo "here is :$(pwd)"
+echo "2 $mydate" > $file
