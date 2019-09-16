14ebde3432ad74ed5bc6d9a61e5a04aabe87cf1c
greatabel <myreceiver2for2github@gmail.com>
2019-09-17 00:08:45 +0800

    demo of zadd

--- RedisDeepAdventure/i0zset_demo.py
+++ RedisDeepAdventure/i0zset_demo.py
@@ -14,9 +14,39 @@ zset 可以存储粉丝列表，value值是粉丝id，score是关注时间，
 import time
 from termcolor import colored
 
+import redis
+
+ 
+
+# Create a redis client
+
+redis_client = redis.StrictRedis(host='localhost',
+                                port=6379,
+                                db=0)
 
 def main_process():
-    print(colored('mycount=', 'red'), 'results')
+    books = "books"
+
+    bookname = 'think in java'
+    score = 9.0
+    book = {bookname: score}
+    redis_client.zadd(books, book)
+
+    bookname = 'python cookbook'
+    score = 9.5
+    book = {bookname: score}
+    redis_client.zadd(books, book)
+
+    bookname = 'php in action'
+    score = 5.0
+    book = {bookname: score}
+    redis_client.zadd(books, book)
+
+    # Print all the players based on the score in ascending order
+    print(colored('Contents of the Redis sorted set in ascending order:', 'red'))
+    print(redis_client.zrange(books, 0, -1))
+
+
 
 if __name__ == "__main__":
     tic = time.process_time()
