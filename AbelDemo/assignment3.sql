cb13d1f13cc67d166e6a66ac4003042cfb225d69
greatabel <myreceiver2for2github@gmail.com>
2020-09-25 15:49:08 +0800

    p1-p10

--- AbelDemo/assignment3.sql
+++ AbelDemo/assignment3.sql
@@ -105,20 +105,102 @@ where not(w.salary <=  (select min(w1.salary)
 
 	      
 -- part 3  problem 5
-drop VIEW if exists p_skill_view;
-create view p_skill_view as
-	select p.pid , count(*)  from personskill p 
-	having count(*) >= 3
-	group by p.pid;
-	      
 drop VIEW if exists people_with_3jobs_view;
+drop VIEW if exists p_skill_view;
+drop VIEW if exists p_skill_without3_view;
 
-CREATE VIEW people_with_3jobs_view AS
-SELECT knows.pid1 
-FROM  knows left join p_skill_view
+create view p_skill_view as
+	select p.pid , count(*) as num from personskill p 	
+	group by p.pid
+	having count(*) >= 3;
+
+create view p_skill_without3_view as
+	select p.pid , count(*) as num from personskill p 	
+	group by p.pid
+	having count(*) = 2
+    order by p.pid;
+
+
+create view people_with_3jobs_view AS
+select knows.pid1 , knows .pid2
+from  knows  inner join p_skill_view
 on knows.pid2 = p_skill_view.pid;
 
-select p.pid, p.name from Person p left join people_with_3jobs_view
-	on p.pid = people_with_3jobs_view.pid1 
-where city = 'Bloomington' 
-     
+select distinct p.pid, p.name from Person p 
+where p.city = 'Bloomington' and p.pid not in 
+(select pid1 from people_with_3jobs_view 
+group by pid1
+having count(*) > 1
+);
+ 
+-- part 3  problem 6
+select distinct p.pid, p.name from Person p 
+where  p.pid  in 
+(select pid from p_skill_without3_view 
+) order by p.pid;
+
+-- part 3  problem 7
+drop VIEW if exists know_company_view;
+
+create view know_company_view as
+	select knows.pid1, knows.pid2 , worksfor.cname as pid2_cname from knows inner join worksfor
+	on knows.pid2 = worksfor.pid ;
+
+
+(select w.cname   ,count(*) as num from worksfor w  join know_company_view k2 
+on w.pid = k2.pid1 
+where w.cname = pid2_cname
+group by w.pid
+having count(*) >=2
+ 
+)
+union
+(select distinct w.cname ,0 as num from worksfor w left join know_company_view k2 
+on w.pid = k2.pid1 
+where w.cname <> pid2_cname
+group by w.pid
+
+) order by cname ;
+
+
+-- part 3  problem 8
+drop VIEW if exists salary_above_55000;
+
+create view salary_above_55000 as
+	select knows.pid1, knows.pid2 , worksfor.salary  from knows inner join worksfor
+	on knows.pid2 = worksfor.pid 	
+	where worksfor.salary > 55000;
+
+select pid ,name from person p left join salary_above_55000
+on p.pid = salary_above_55000.pid1 
+where pid2 is null order by pid ;
+
+
+-- part 3  problem 9
+drop VIEW if exists p9_view;
+
+create view p9_view as
+	select knows.pid1 as pid  ,knows.pid2 , worksfor.salary, p2.birthyear  from (knows inner join worksfor
+	on knows.pid2 = worksfor.pid ) 	inner join person p2 on worksfor.pid = p2.pid
+	where worksfor.cname = 'Netflix' and worksfor.salary >= 55000 
+		and p2.birthyear > 1985;
+
+select p.pid , p.name from person p inner join p9_view
+on p.pid = p9_view.pid order by p.pid;
+
+
+-- part 3  problem 10
+drop VIEW if exists p10_view;
+
+create view p10_view as
+	(select distinct cname from worksfor	
+	where worksfor.salary < 55000)
+EXCEPT
+	(select distinct cname from worksfor	
+	where worksfor.salary >= 55000);
+
+select * from p10_view;
+
+-- part 3  problem 11
+
+
