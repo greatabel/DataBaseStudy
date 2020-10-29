4b2e7b2ce4607ebf45a77cb4ad9b3c1cfba28fa5
greatabel <myreceiver2for2github@gmail.com>
2020-10-29 23:45:10 +0800

    import all 4csv into sqlite3

---
+++ a6754DataPreProcessing/scaffolding/source_dataset/creata_db_schema.sql
@@ -0,0 +1,70 @@
+CREATE TABLE Book1 (
+id INTEGER PRIMARY KEY, 
+title TEXT, 
+authors TEXT, 
+pubyear TEXT, 
+pubmonth TEXT, 
+pubday TEXT, 
+edition TEXT, 
+publisher TEXT, 
+isbn13 TEXT, 
+language TEXT, 
+series TEXT, 
+pages INTEGER
+)
+
+
+CREATE TABLE Book2 (
+id INTEGER PRIMARY KEY,
+book_title TEXT,
+authors TEXT,
+publication_year TEXT,
+publication_month TEXT,
+publication_day TEXT,
+edition TEXT,
+publisher_name TEXT,
+isbn13 TEXT,
+language TEXT,
+series TEXT,
+pages INTEGER
+)
+
+
+CREATE TABLE Book3 (
+ID INTEGER PRIMARY KEY,
+Title TEXT,
+Author1 TEXT,
+Author2 TEXT,
+Author3 TEXT,
+Publisher TEXT,
+ISBN13 TEXT,
+Date TEXT,
+Pages TEXT,
+ProductDimensions TEXT,
+SalesRank TEXT,
+RatingsCount TEXT,
+RatingVal TEXT,
+PaperbackPrice TEXT,
+HardcoverPrice TEXT,
+EbookPrice TEXT,
+AudiobookPrice  TEXT
+)
+
+CREATE TABLE Book4 (
+ID TEXT PRIMARY KEY,
+Title TEXT,
+UsedPrice TEXT,
+NewPrice TEXT,
+Author TEXT,
+ISBN10 TEXT,
+ISBN13 TEXT,
+Publisher TEXT,
+Publication_Date TEXT,
+Pages TEXT,
+Dimensions TEXT
+)
+
+
+
+
+
---
+++ a6754DataPreProcessing/scaffolding/source_dataset/readme.md
@@ -0,0 +1,10 @@
+1. touch book.db
+
+2. create tables in db by create_db_schema.sql
+
+3.
+.import Book1.csv Book1
+或者
+sqlite3 book.db -separator ',' '.import Book3.csv Book3'
+
+
